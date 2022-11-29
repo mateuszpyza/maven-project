b84043188364a5dcaa600fe0e3d29fbe70833653
mateuszpyza <mateuszpyza1@wp.pl>
2022-11-29 23:03:44 +0100

    fixed calculator

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,9 +1,6 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 class CalculatorTest {
 
     @Test
